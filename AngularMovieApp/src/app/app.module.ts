550e92b4f9b2bdfe272fe7d297a91d25b9935fa1
Musa Gumede <gracegumedem@gmail.com>
2025-10-23 15:37:47 +0200

    commented out error in search component html

--- AngularMovieApp/src/app/app.module.ts
+++ AngularMovieApp/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
+import { HeaderComponent } from './header/header.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { MovieSearchComponent } from './movie-search/movie-search.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieCardComponent,
+    HeaderComponent,
+    FavoritesComponent,
+    MovieSearchComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
